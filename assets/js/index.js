f27da151fb1617f9ef7126d3be8517d75bef92cc
anshtyagi0 <tyagiansh175@gmail.com>
2025-06-13 14:20:55 +0530

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,7 +27,7 @@ function validateLogin() {
     const email = document.getElementById('email').value;
     const pass = document.getElementById("password").value;
     const url = 'https://ssplbackend.anshtyagi.com/api/login';
-
+    // am
     fetch(url, {
         method: 'POST',
         headers: {
